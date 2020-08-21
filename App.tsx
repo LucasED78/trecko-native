16b43bf79da147d48efaa88f1fc16abd5f98c8db
Lucas Eduardo <lucased78@hotmail.com>
2020-08-20 22:17:53 -0300

    feat: integrating components with the order service

--- App.tsx
+++ App.tsx
@@ -1,19 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, ToastAndroid, ActivityIndicator } from 'react-native';
 import UnderlineInput from './src/components/Input/UnderlineInput';
-import RoundedButton from './src/components/Button/RoundedButton';
-import OrderTitle from './src/components/Order/OrderTitle';
-import OrderDescription from './src/components/Order/OrderDescription';
+import OrderServiceImpl from './src/services/order/OrderServiceImpl';
+import { Order, OrderTracks } from './src/models/Order';
+import OrderList from './src/components/Order/OrderList';
+import IconButton from './src/components/Button/IconButton';
+import VerticalContainer from './src/components/VerticalContainer/VerticalContainer';
 
 export default function App() {
+  const [order, setOrder] = useState<Order | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [code, setCode] = useState<string>("");
+
+  const search = async () => {
+    if (!code) {
+      return ToastAndroid.show("O código deve ser preenchido", ToastAndroid.SHORT);
+    }
+
+    try {
+      setLoading(true);
+
+      const response = await new OrderServiceImpl()
+        .get(code);
+
+      const order = new Order(
+        response.code,
+        response.tracks.map((res: any) => new OrderTracks(
+          res.locale,
+          res.status,
+          res.trackedAt
+        ))
+      );
+
+      setOrder(order);
+
+      setLoading(false);
+    } catch(_) {
+      ToastAndroid.show("Ocorreu um erro ao tentar rastrear o código! Tente novamente", ToastAndroid.SHORT);
+    }
+  }
+
+  const clear = () => {
+    setCode("");
+    setOrder(null);
+  }
+
   return (
     <SafeAreaView>
-      <UnderlineInput placeholder="Digite o código de rastreio" />
-      <RoundedButton text="rastrear" onPress={() => {}} />
-      <OrderTitle title="objeto entregue ao destinatário" />
-      <OrderDescription description="12/05/2020 ás 10:00:00" />
-      <OrderDescription description="12/05/2020 ás 10:00:00" />
+      <VerticalContainer>
+        <VerticalContainer 
+          style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginVertical: 10 }}>
+          <UnderlineInput
+            onSuffixPressed={() => clear()}
+            placeholder="Digite o código de rastreio"
+            value={code}
+            onChange={e => setCode(e.nativeEvent.text)} />
+          <IconButton icon="ios-search" onPress={search} />
+        </VerticalContainer>
+        <>
+          {loading && <ActivityIndicator size="large" color="#00B0FF"/>}
+          {order?.code && <OrderList order={order}/> }
+        </>
+      </VerticalContainer>
     </SafeAreaView>
   );
 }
